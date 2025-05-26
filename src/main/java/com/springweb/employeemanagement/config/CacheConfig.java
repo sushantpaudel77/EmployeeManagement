1791053dba67b74b7979fc5c7670cae30474f605
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-05-26 21:08:36 +0545

    manage serialization for redis

--- src/main/java/com/springweb/employeemanagement/config/CacheConfig.java
+++ src/main/java/com/springweb/employeemanagement/config/CacheConfig.java
@@ -1,11 +1,40 @@
 package com.springweb.employeemanagement.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @EnableCaching
 @Configuration
 public class CacheConfig {
 
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper javaTimeModuleMapper = objectMapper.registerModule(new JavaTimeModule());
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(javaTimeModuleMapper);
+
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .prefixCacheNameWith("my-redis-")
+                .entryTtl(Duration.ofSeconds(60))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(configuration)
+                .build();
+    }
 }
